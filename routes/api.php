dd73416d890b34f2d134f4844ceac7562b8b6923
minzawdev <minzaw.webdev@gmail.com>
2023-09-05 15:37:54 +0630

    fixed logout api

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\API\V1\FileVerificationController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class,'logout']);
 
 Route::middleware('auth:api')->group( function () {
     Route::prefix('v1')->group(function () {
         Route::post('/verifiable_file',[FileVerificationController::class,'verifyFile']);
         Route::get('/file_analysis',[FileVerificationController::class,'getAnalysisFile']);
     });
+    Route::post('logout', [AuthController::class,'logout']);
 });
